719bed7865ee68587a8143b8e27745e9378051d7
danrkeith <77320308+danrkeith@users.noreply.github.com>
2022-01-22 16:11:49 +1100

    completed implementation of rotation system

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerInput : MonoBehaviour
+{
+    private Tetromino activeTet;
+
+    private void Awake()
+    {
+        // Initiate components
+        GameObject activePiece = GameObject.FindGameObjectWithTag("Active");
+        activeTet = activePiece.GetComponent<Tetromino>();
+    }
+
+    public void OnRotate(InputValue value)
+    {
+        activeTet.Rotate((int)value.Get<float>());
+    }
+}
---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+    public Sprite[] frames;
+    private SpriteRenderer sr;
+    private int frameIndex = 0;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void Rotate(int value)
+    {
+        transform.Rotate(0, 0, value * 90);
+
+        frameIndex += value;
+        switch (frameIndex)
+        {
+            case -1:
+                frameIndex = 3;
+                break;
+            case 4:
+                frameIndex = 0;
+                break;
+            default:
+                break;
+        }
+        sr.sprite = frames[frameIndex];
+    }
+}
