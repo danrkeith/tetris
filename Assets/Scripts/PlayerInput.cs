157332dc576d1d46fdf00c39ab9f485eae212af0
danrkeith <77320308+danrkeith@users.noreply.github.com>
2022-01-24 13:23:31 +1100

    converted to tile-based model

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -6,44 +6,21 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerInput : MonoBehaviour
 {
-    const int DASFRAMES = 16;
-    const int ASFRAMES = 2;
-
     private Tetromino activeMino;
-    private int dir = 0;
 
     private void Awake()
     {
-        // Initiate components
+        // Find active tetromino
         activeMino = GameObject.FindWithTag("Active").GetComponent<Tetromino>();
     }
 
-    public void OnRotate(InputValue value)
+    private void OnRotate(InputValue value)
     {
-        activeMino.Rotate((int)value.Get<float>());
+        activeMino.Rotate(value.Get<float>() * 90);
     }
     
-    public void OnMove(InputValue value)
+    private void OnMove(InputValue value)
     {
-        dir = (int)value.Get<float>();
-
-        if (dir != 0)
-        {
-            StartCoroutine(Move(dir));
-        }
-    }
-
-    private IEnumerator Move(int value)
-    {
-        // Initial movement w/ DAS
-        activeMino.transform.Translate(new Vector2(value * 0.4f, 0), Space.World);
-        yield return new WaitForSeconds(DASFRAMES / 60f);
-
-        // Held movement after, check for release
-        while (dir == value)
-        {
-            activeMino.transform.Translate(new Vector2(value * 0.4f, 0), Space.World);
-            yield return new WaitForSeconds(ASFRAMES / 60f);
-        }
+        activeMino.StartMovement(value.Get<float>());
     }
 }
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -4,47 +4,60 @@ using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
-    public Sprite[] frames;
-    private SpriteRenderer sr;
-    private int frameIndex = 0;
+    const int DASFRAMES = 16;
+    const int ASFRAMES = 2;
 
-    private void Awake()
-    {
-        sr = GetComponent<SpriteRenderer>();
-    }
+    private int fallFrames = 48;
+    private int xVelocity = 0;
 
     private void Start()
     {
-        StartCoroutine(nameof(Lower));
+        StartCoroutine(nameof(Fall));
     }
 
-    private IEnumerator Lower()
+    public void Rotate(float angle)
+    {
+        transform.Rotate(0, 0, angle);
+        foreach (Transform child in transform)
+        {
+            child.Rotate(0, 0, -angle);
+        }
+    }
+
+    public void StartMovement(float newXVelocity)
+    {
+        xVelocity = (int)newXVelocity;
+
+        if (xVelocity != 0)
+        {
+            StartCoroutine(nameof(AutoShift));
+        }
+    }
+
+    private IEnumerator AutoShift()
+    {
+        int coroutineXVelocity = xVelocity;
+
+        void Move() => transform.Translate(new Vector2(coroutineXVelocity * 0.4f, 0), Space.World);
+
+        // Initial movement
+        Move();
+        yield return new WaitForSeconds(DASFRAMES / 60f);
+
+        // Autoshift until velocity has changed
+        while (coroutineXVelocity == xVelocity)
+        {
+            Move();
+            yield return new WaitForSeconds(ASFRAMES / 60f);
+        }
+    }
+    
+    private IEnumerator Fall()
     {
         while (true)
         {
             transform.Translate(new Vector2(0, -0.4f), Space.World);
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(fallFrames / 60f);
         }
-    }
-
-    public void Rotate(int value)
-    {
-        transform.Rotate(0, 0, value * 90);
-
-        frameIndex += value;
-
-        // Loop index
-        switch (frameIndex)
-        {
-            case -1:
-                frameIndex = 3;
-                break;
-            case 4:
-                frameIndex = 0;
-                break;
-            default:
-                break;
-        }
-        sr.sprite = frames[frameIndex];
     }
 }
