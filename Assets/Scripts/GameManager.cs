76be11bac6202fbcdc1764a8069d7af30c45b6c6
danrkeith <77320308+danrkeith@users.noreply.github.com>
2022-01-27 16:02:45 +1100

    added all pieces and created main spawn cycle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject[] tetrominoes;
+    private List<int> _minoQueue;
+
     public Tetromino ActiveMino { get; private set; }
 
     private void Awake()
     {
-        SetActiveMino();
+        ResetMinoQueue();
+
+        SpawnMino();
     }
 
     public void SpawnMino()
     {
-        Debug.Log("SpawnNewMino");
+        int i = Random.Range(0, _minoQueue.Count);
+        Instantiate(tetrominoes[_minoQueue[i]]);
+
+        _minoQueue.RemoveAt(i);
+
+        // Refill queue if necessary
+        if (_minoQueue.Count == 0)
+        {
+            ResetMinoQueue();
+        }
+
+        SetActiveMino();
     }
 
     private void SetActiveMino()
     {
         ActiveMino = GameObject.FindWithTag(Tetromino.Active).GetComponent<Tetromino>();
+    }
+
+    private void ResetMinoQueue()
+    {
+        _minoQueue = new List<int>();
+        for (int i = 0; i < tetrominoes.Length; i++)
+        {
+            _minoQueue.Add(i);
+        }
     }
 }
